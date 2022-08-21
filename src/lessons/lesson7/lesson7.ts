632050fcb7e3c7bbebbcca3fdf78473c17784356
Vladimir Traihel <vovatraigel@gmail.com>
2022-08-21 18:22:26 +0300

    task lesson 7 - __Proto__ , Prototype

--- src/lessons/lesson7/lesson7.ts
+++ src/lessons/lesson7/lesson7.ts
@@ -20,26 +20,119 @@ console.log('Lesson 7');
 // walk => `${this.name} walking`
 // проверить, что методы работают
 
+class Animal {
+    constructor(name: string = 'Animal') {
+        this.name = name;
+    };
+    name: string = '';
+    walk() {
+        console.log(`${this.name} walking`)
+    };
+    eat() {
+        console.log(`${this.name} eating`)
+    };
+    sleep() {
+        console.log(`${this.name} sleeping`)
+    }
+}
+
+const dog = new Animal('Pet')
+console.log(dog.walk())
+
 
 //Task 02
 // Реализовать класс Monkey на базе класса Animal,  конструктор принимает name(по умолчанию 'Monkey') в качестве
 // параметра, реализовать методы roar и climb аналогично классу Animal
 // проверить, что все методы работают
 
+class Monkey extends Animal {
+    roar() {
+        console.log(`${this.name} roaring`)
+    }
+    climb() {
+        console.log(`${this.name} climbing`)
+    }
+}
+
+const monk = new Monkey('Monk')
+console.log(monk.roar())
 
 //Task 03
 // Реализовать класс Human на базе класса Monkey, конструктор принимает name(по умолчанию 'Human') в качестве
 // параметра, реализовать методы speak и think аналогично классу Animal
 // проверить, что все методы работают
 
+class Human extends Monkey {
+    speak() {
+        console.log(`${this.name} speaking`)
+    }
+    think() {
+        console.log(`${this.name} thinking`)
+    }
+}
+
+const alex = new Human('Alex')
+console.log(alex.think())
 
 // Task 04
 // Реализовать таски 01-03 через функции конструкторы в отдельном JS файле, реализовать наследование
 
+//========== task 1 =========
+function AnimalFun(name: string = 'Animal') {
+    // @ts-ignore
+    this.name = name;
+}
+
+AnimalFun.prototype.walk = function () {
+    console.log(`${this.name} walking`)
+};
+AnimalFun.prototype.eat = function () {
+    console.log(`${this.name} eating`)
+};
+AnimalFun.prototype.sleep = function () {
+    console.log(`${this.name} sleeping`)
+};
+
+console.log(AnimalFun)
+
+//=========== task 2 ==========
+function MonkeyFun(name ='Monkey') {
+    // @ts-ignore
+    this.name = name;
+}
+
+MonkeyFun.prototype.__proto__ = AnimalFun.prototype
+
+MonkeyFun.prototype.roar = function() {
+    console.log(`${this.name} roaring`)
+}
+
+MonkeyFun.prototype.climb = function() {
+    console.log(`${this.name} climbing`)
+}
+
+console.log(MonkeyFun)
+
+//=========== task 3 ==========
+
+function HumanFun(name ='Human') {
+    // @ts-ignore
+    this.name = name;
+}
+
+HumanFun.prototype.__proto__ = MonkeyFun.prototype
+
+HumanFun.prototype.speak = function() {
+    console.log(`${this.name} speaking`)
+}
+HumanFun.prototype.think = function() {
+    console.log(`${this.name} thinking`)
+}
+
+console.log(HumanFun)
 
 // Task 05
 // Используя метод Apply реализовать свой собственный метод bind
-
 
 // just a plug
 export default () => {};
---
+++ src/native-JS/lesson_13.js
@@ -0,0 +1,44 @@
+// prototype / __proto__
+
+
+//__proto__ - есть у всех объектов и ссылается на prototype с помощью которого объект был создан.
+// prototype - это объект, который есть у class, либо function (у стрелочных функций его нет),
+
+// __proto__ - есть у всех объектов.
+// Почти всегда это объект. При том разые __proto__ разных по 'типу' объектов - они равны, то есть это один и тот же объект.
+
+
+//const man = {};   // new Object(...)
+// const man2 = {};
+// man.__proto__ === man2.__proto__   // true
+//
+// const users = [];   // new Array(...)
+// const users2 = [];
+// users.__proto__ === users2.__proto__   // true
+//
+// const str = "string";   // new String(...)
+// const str2 = "string2";
+// str.__proto__ === str2.__proto__   // true
+//
+// man.__proto__ === users.__proto__   //false
+// users.__proto__ === str.__proto__   //false
+
+
+//__proto__ любого объекта ссылается на prototype клвсса (функцию-конструктора), с помощью которой этот объект был создан (сконструирован)
+
+// prototype - есть у class, либо function (у стрелочных функций его нет)
+
+// Каждый prototype - это незавичимый объект, сам по себе, с определённым набором свойст и методов.
+
+
+//const promise = new Promise(()=>{});
+// promise.__proto__ === Promise.prototype   // true
+//
+// const man = {}; // new Object(...)
+// man.__proto__ === Object.prototype   //true
+//
+// const users = []; // new Array(...)
+// users.__proto__ === Array.prototype   //true
+//
+// const str = "string"; // new String(...)
+// str.__proto__ === String.prototype   //true
